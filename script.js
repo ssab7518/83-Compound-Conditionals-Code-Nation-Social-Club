4c360012063f25062b49159095de831da3d1c07f
ssab7518 <76485192+ssab7518@users.noreply.github.com>
2022-12-20 08:59:16 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+// set the correct username and password combination below
+let correctUsername = "hey";
+let correctPassword = "hi";
+
+
+$("button").click(function() {
+    let username = $(".username").val();
+    let password = $(".password").val();
+    
+    // WRITE YOUR COMPOUND CONDITIONAL BELOW THIS LINE
+if (username ==="hey"){
+$(".message").text("Correct!");}
+
+
+});
